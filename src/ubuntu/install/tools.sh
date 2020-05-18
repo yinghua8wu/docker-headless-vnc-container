d17aa8adfdcff2f6c7f687f75eb3356f73c6ca25
yinghua8wu <38562798+yinghua8wu@users.noreply.github.com>
2020-05-19 00:43:34 +0800

    Update tools.sh

--- src/ubuntu/install/tools.sh
+++ src/ubuntu/install/tools.sh
@@ -3,8 +3,8 @@
 set -e
 
 echo "Install some common tools for further installation"
-apt-get update && apt-get install -y vim wget net-tools locales bzip2 screen corn inetutils-ping tmate \
-    python-numpy #used for websockify/novnc
+apt-get update && apt-get install -y vim wget net-tools locales bzip2 screen corn inetutils-ping tmate python-numpy
+#used for websockify/novnc
 apt-get clean -y
 
 echo "generate locales für en_US.UTF-8"
